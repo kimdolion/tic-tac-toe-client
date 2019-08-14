5512a345c07b57883421aaee6afa97bd609321a0
Kimberly Wilkes <49125411+kimdolion@users.noreply.github.com>
2019-08-13 20:54:06 -0400

    Revert "Add authentication feature"

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,8 +3,7 @@
 let apiUrl
 const apiUrls = {
   production: 'https://tic-tac-toe-wdi-production.herokuapp.com',
-  development: 'https://tic-tac-toe-wdi.herokuapp.com/'
-  // development: 'http://localhost:4741'
+  development: 'http://localhost:4741'
 }
 
 if (window.location.hostname === 'localhost') {
